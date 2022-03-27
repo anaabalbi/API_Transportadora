2ea00bcb36b5129cbadc11b2909b36c39dca2855
anaabalbi <anaabalbi@gmail.com>
2022-03-27 15:31:46 -0300

    chamando a address-controller.js

--- src/express.js
+++ src/express.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import addressController from './controllers/address-controller.js';
+import database from './database/sqlite-db.js';
 
 const app = express();
 
 app.use(express.json());
+
+addressController(app, database);
 
 export default app;

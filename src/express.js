0b181b313db11743241f33e14cea8a5ba12443cc
anaabalbi <anaabalbi@gmail.com>
2022-04-22 19:22:21 -0300

    instalando cors

--- src/express.js
+++ src/express.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import addressController from './controllers/address-controller.js';
 import database from './database/sqlite-db.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 addressController(app, database);
 
 export default app;
